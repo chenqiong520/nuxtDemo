5c10bab154e35fd7bea17b4b11f62afd05296404
chenq1024 <741718030@qq.com>
2019-03-12 11:15:47 +0800

    热门课程组件

--- pages/index.vue
+++ pages/index.vue
@@ -1,106 +1,128 @@
 <template>
   <div class="home">
-    <banner/>
+    <banner />
     <!--今日公开课-->
     <div class="public-course">
       <div class="course-title-box clear">
         <div class="left">
-          <div><img src="@/assets/img/home/public-icon.png"/><span class="course-title">今日公开课</span></div>
-          <div class="title-desc">提前了解技术大牛精心研发课程</div>
+          <div><img src="@/assets/img/home/public-icon.png"><span class="course-title">今日公开课</span></div>
+          <div class="title-desc">
+            提前了解技术大牛精心研发课程
+          </div>
         </div>
         <div class="right">
-          <i  class="iconfont iconarrow-right-copy-copy-copy" style="font-size:32px; color:#444"></i>
+          <i class="iconfont iconarrow-right-copy-copy-copy" style="font-size:32px; color:#444" />
         </div>
       </div>
       <div class="course-cont-wrapper clear">
         <div class="left-item">
           <div class="course-img">
-            <img src="//cdn.gupaoedu.com/newIndex/images/stk/Java1.jpg"/>
-            <div class="status-desc">直播中</div>
+            <img src="//cdn.gupaoedu.com/newIndex/images/stk/Java1.jpg">
+            <div class="status-desc">
+              直播中
+            </div>
           </div>
-          <div class="course-name">JAVA开发架构课</div>
-          <div class="course-teacher">咕泡学院-jack老师</div>
+          <div class="course-name">
+            JAVA开发架构课
+          </div>
+          <div class="course-teacher">
+            咕泡学院-jack老师
+          </div>
         </div>
         <div class="right-item">
           <div class="course-img">
-            <img src="//cdn.gupaoedu.com/newIndex/images/stk/ai.jpg"/>
-            <div class="status-desc">直播中</div>
+            <img src="//cdn.gupaoedu.com/newIndex/images/stk/ai.jpg">
+            <div class="status-desc">
+              直播中
+            </div>
           </div>
-          <div class="course-name">人工智能</div>
-          <div class="course-teacher">咕泡学院-秦老师</div>
+          <div class="course-name">
+            人工智能
+          </div>
+          <div class="course-teacher">
+            咕泡学院-秦老师
+          </div>
         </div>
       </div>
     </div>
     <!--热门课程-->
-    <div class="hot-course">
-      <div class="course-title-box clear">
-        <div class="left">
-          <div><img src="@/assets/img/home/hot-icon.png"/><span class="course-title">热门课程</span></div>
-          <div class="title-desc">做技术人的指路明灯，职业生涯的精神导师</div>
-        </div>
-        <div class="right">
-          <i  class="iconfont iconarrow-right-copy-copy-copy" style="font-size:32px; color:#444"></i>
-        </div>
+    <hot-course :hotCourse="hotCourse" />
+    <!--8s服务体系-->
+    <div class="service-system">
+      <div class="category-title">
+        <div><span class="left-line" />首创在线教育8S服务体系<span class="right-line" /></div>
+        <p class="text">
+          只为更好的你
+        </p>
       </div>
-      <div class="course-cont-wrapper clear">
-        <div class="left-item">
-          <div class="course-img">
-            <img src="//cdn.gupaoedu.com/newIndex/images/kc/Git.jpg"/>
-            <div class="num status-desc">
-              <span class="span1"><i class="iconfont iconread"> </i>1021</span>
-              <span><i class="iconfont iconcomment"> </i>265</span>
-            </div>
-          </div>
-          <div class="course-name">Git入门教程</div>
-          <div class="price">¥29.99</div>
-        </div>
-        <div class="right-item">
-          <div class="course-img">
-            <img src="//cdn.gupaoedu.com/newIndex/images/kc/xmkjpkc.jpg"/>
-            <div class="status-desc">直播中</div>
-          </div>
-          <div class="course-name">小马哥的精品课程</div>
-          <div class="price">¥29.99</div>
-        </div>
-      </div>
-      <div class="course-cont-wrapper clear">
-        <div class="left-item">
-          <div class="course-img">
-            <img src="//cdn.gupaoedu.com/newIndex/images/stk/Java1.jpg"/>
-            <div class="status-desc">直播中</div>
-          </div>
-          <div class="course-name">JAVA开发架构课</div>
-          <div class="course-teacher">咕泡学院-jack老师</div>
-        </div>
-        <div class="right-item">
-          <div class="course-img">
-            <img src="//cdn.gupaoedu.com/newIndex/images/stk/ai.jpg"/>
-            <div class="status-desc">直播中</div>
-          </div>
-          <div class="course-name">人工智能</div>
-          <div class="course-teacher">咕泡学院-秦老师</div>
-        </div>
-      </div>
+      <table width="100%" cellspacing="0" cellpadding="0">
+        <tr>
+          <td>
+            <div></div>
+            <div>入学评估</div>
+          </td>
+          <td>
+            <div></div>
+            <div>学籍管理</div>
+          </td>
+          <td>
+            <div></div>
+            <div>监督学习</div>
+          </td>
+          <td>
+            <div></div>
+            <div>就业内推</div>
+          </td>
+        </tr>
+      </table>
     </div>
-    <student/>
-    <teacher/>
+    <student />
+    <teacher />
   </div>
 </template>
 
 <script>
 import Banner from './home/banner'
+import HotCourse from '@/components/HotCourse'
 import Student from './home/student'
 import Teacher from './home/teacher'
 
 export default {
   name: 'Index',
-
   layout: 'home',
 
   components: {
     Banner,
+    HotCourse,
     Student,
     Teacher
+  },
+  data() {
+    return {
+      hotCourse: {
+        titleIcon: '/_nuxt/assets/img/home/hot-icon.png',
+        titleName: '热门课程',
+        titleDesc: '做技术人的指路明灯，职业生涯的精神导师',
+        courseList: [
+          {
+            coverImg: '//cdn.gupaoedu.com/newIndex/images/kc/Git.jpg',
+            url: '',
+            readNum: 14363,
+            CommentNum: 460,
+            courseName: ' Git入门教程',
+            price: '¥29.99'
+          },
+          {
+            coverImg: '//cdn.gupaoedu.com/newIndex/images/kc/xmkjpkc.jpg',
+            url: '',
+            readNum: 6112,
+            CommentNum: 20,
+            courseName: ' 小马哥的精品课程',
+            price: ' ¥6.66'
+          }
+        ]
+      }
+    }
   }
 }
 </script>
@@ -156,7 +178,7 @@ export default {
   width: 334px;
   height: 186px;
 }
-.course-cont-wrapper .course-img>img {
+.course-cont-wrapper .course-img > img {
   width: 100%;
   height: 100%;
   background: rgba(77, 61, 238, 1);
@@ -198,5 +220,47 @@ export default {
   line-height: 32px;
   font-weight: bold;
   color: rgba(253, 85, 58, 1);
+}
+.hot-course .num .span1 {
+  display: inline-block;
+  margin-right: 24px;
+}
+.hot-course .num .iconfont {
+  font-size: 14px;
+  margin-right: 5px;
+}
+.service-system {
+  background: rgba(248, 248, 248, 1);
+  padding-top: 48px;
+}
+.category-title {
+  font-size: 36px;
+  line-height: 36px;
+  font-weight: bold;
+  color: rgba(37, 37, 37, 1);
+  text-align: center;
+}
+.category-title .left-line {
+  width: 35px;
+  height: 4px;
+  background: rgba(222, 222, 222, 1);
+  margin-right: 16px;
+  display: inline-block;
+  vertical-align: middle;
+}
+.category-title .right-line {
+  width: 35px;
+  height: 4px;
+  background: rgba(222, 222, 222, 1);
+  margin-left: 16px;
+  display: inline-block;
+  vertical-align: middle;
+}
+.category-title .text {
+  font-size: 24px;
+  font-weight: 400;
+  color: rgba(153, 153, 153, 1);
+  line-height: 24px;
+  margin-top: 18px;
 }
 </style>
