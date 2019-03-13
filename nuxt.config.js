1e5048b8ea6cfd3e670d297b75a02e2eeec9df4d
tianjiannan <470812009@qq.com>
2019-03-13 18:19:28 +0800

    冲突

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,154 +1,23 @@
-module.exports = {
-  mode: 'universal',
+const merge = require('webpack-merge')
+const baseConfig = require('./config/base.config')
+const devConfig = require('./config/dev.config')
+const prodConfig = require('./config/prod.config')
+const testConfig = require('./config/test.config')
 
-  /*
-   ** 多页面的head
-   */
-  head: {
-    title: '咕泡学院',
-    meta: [{
-        charset: 'utf-8'
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: '咕泡学院是一家专注IT互联网在线教育培训机构,提供java架构师培训,人工智能培训,大数据培训,软件测试培训,Web前端培训,区块链培训等课程，课程均由一线互联网公司技术大牛精心研发，汇集多年工作积累，让您站在巨人的肩膀上快速成长。学院宗旨：做技术人的指路明灯，职场生涯的精神导师！"'
-      }
-    ],
-    link: [{
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
-      },
-      /**
-       * 阿里iconfont
-       * 开发时，可使用阿里提供的地址
-       * 上线时，替换为下载好的本地字体
-       */
-      {
-        rel: 'stylesheet',
-        href: 'http://at.alicdn.com/t/font_1078836_k9ya6yrxnwd.css'
-      }
-    ],
+const env = process.env.NODE_ENV
+console.log(env)
 
-    // script: [{
-    //   src: 'http://g.tbcdn.cn/mtb/lib-flexible/0.3.4/??flexible_css.js,flexible.js'
-    // }]
-  },
+// 默认为开发环境
+let mergedConfig = merge(baseConfig, devConfig)
 
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: {
-    color: '#fff'
-  },
-
-  /*
-   ** 全局css
-   */
-  css: [
-    '@/assets/css/reset.css',
-    'swiper/dist/css/swiper.css'
-  ],
-
-  /*
-   ** 第三方插件
-   */
-  plugins: [{
-    src: '~/plugins/swiper.js',
-    ssr: false
-  }, {
-    src: '~/plugins/filters.js',
-    ssr: false
-  }, {
-    src: '~/plugins/cipboard.js',
-    ssr: false
-  }],
-
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/proxy'
-  ],
-
-  /*
-   ** Axios module configuration
-   */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-  },
-
-  /**
-   * 配置开发环境代理
-   */
-  proxy: [
-    [
-      '/wwwapi',
-      {
-        target: 'http://192.168.8.122', // 测试服务器
-        changeOrigin: true, // 是否跨域
-        pathRewrite: { // 重写path
-          '^/wwwapi': '/manageapi/wwwapi' //将 /wwwapi 代替为 /manageapi/wwwapi
-        }
-      }
-    ],
-    [
-      '/arrangement',
-      {
-        target: 'http://192.168.8.122', // 测试服务器
-        changeOrigin: true, // 是否跨域
-        pathRewrite: { // 重写path
-          '^/arrangement': '/manageapi/arrangement'
-        }
-      }
-    ]
-  ],
-
-  /** 
-   * 打包配置
-   */
-  build: {
-
-    // extend(config, ctx) {
-    //   // 编译时进行eslint检查
-    //   if (ctx.isDev && ctx.isClient) {
-    //     config.module.rules.push({
-    //       enforce: 'pre',
-    //       test: /\.(js|vue)$/,
-    //       loader: 'eslint-loader',
-    //       exclude: /(node_modules)/
-    //     })
-    //   }
-    // },
-
-    // cdn 文件设置
-    // publicPath: 'https://cdn.gupaoedu.com/official-web-mobile',
-
-    postcss: {
-      plugins: {
-        // 'postcss-px2rem': {
-        //   remUnit: 75
-        // }
-        'postcss-px-to-viewport': {
-          viewportWidth: 750, // 视窗的宽度，对应的是我们设计稿的宽度，一般是750 
-          viewportHeight: 1334, // 视窗的高度，根据750设备的宽度来指定，一般指定1334，也可以不配置 
-          unitPrecision: 3, // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除） 
-          viewportUnit: 'vw', // 指定需要转换成的视窗单位，建议使用vw 
-          selectorBlackList: ['.ignore'], // 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名 
-          minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值 
-          mediaQuery: false // 允许在媒体查询中转换`px`
-        }
-      }
-    }
-  },
-
-  generate: {
-    subFolders: false
-  }
+// 测试环境(预生产环境)
+if (env === 'pre-production') {
+  mergedConfig = merge(baseConfig, testConfig)
 }
+
+// 生产环境
+if (env === 'production') {
+  mergedConfig = merge(baseConfig, prodConfig)
+}
+
+module.exports = mergedConfig
